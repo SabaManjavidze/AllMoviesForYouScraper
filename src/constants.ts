3ddb08722cbca0f280dc06cd8b131ab029bc9659
SabaManjavidze <sabamanjavidze@gmail.com>
2022-11-23 18:06:10 +0400

    switched to imovies api

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
+export const IMOVIE_BASE_URL = "https://api.imovies.cc/api/v1";
 export const base_url = "https://allmoviesforyou.net";
 export const episode_url = "ajax/v2/episode/servers";
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { episodeListParse } from "./utils/Episode/episodeListParse";
-import { searchAnimeRequest } from "./utils/Search/searchAnimeRequest";
-import { videoListParse } from "./utils/Video/videoListParse";
 import express from "express";
 import cors from "cors";
+import axios from "axios";
+import { AdjaraMovie, Movie } from "./types";
+import { IMOVIE_BASE_URL } from "./constants";
 
 const app = express();
 
@@ -12,27 +12,65 @@ app.get("/", async (req, res) => {
 });
 app.get("/search/:query", async (req, res) => {
   const { query } = req.params;
-  const movies = await searchAnimeRequest(1, query + "");
-  res.send(movies);
+  try {
+    const movie_filters =
+      "https://api.imovies.cc/api/v1/search-advanced?movie_filters%5B%23%2Fbrowse%2Fmovies%5D=&movie_filters%5Bwith_actors%5D=3&movie_filters%5Bwith_directors%5D=1&filters%5Btype%5D=movie&keywords=inside%20job&page=1&per_page=20";
+    const { data: movies } = await axios.get(
+      `${IMOVIE_BASE_URL}/search-advanced?${movie_filters}&keywords=${query}&page=1&per_page=20`
+    );
+    const parsedMovies = movies.data.map((item: AdjaraMovie) => {
+      return {
+        movieType: item.isTvShow ? "series" : "movies",
+        slug: item?.id?.toString() ?? item?.adjaraId?.toString(),
+        thumbnail_url: item.posters.data[400] ?? item.posters.data[240],
+        title: item.secondaryName,
+      } as Movie;
+    });
+    res.send(parsedMovies);
+  } catch (error) {
+    res.send({ message: "Something went wrong!" });
+  }
 });
 
-app.get("/:movieType/:movieId/episodes", async (req, res) => {
-  const { movieId, movieType } = req.params;
-  if (movieType !== "movies" && movieType !== "series") return;
-  const episodes = await episodeListParse(movieType, movieId + "");
-  res.send(episodes);
-});
-app.get("/video/:movieType/episodeId/:episodeId", async (req, res) => {
-  const { movieType, episodeId } = req.params;
-  if (
-    movieType !== "movies" &&
-    movieType !== "series" &&
-    movieType !== "episode"
-  )
-    return;
+app.get("/details/:movieId", async (req, res) => {
+  const { movieId } = req.params;
 
-  const episode = await videoListParse(movieType, episodeId);
-  res.send(episode);
+  try {
+    const { data: details } = await axios.get(
+      `${IMOVIE_BASE_URL}/movies/${movieId}`
+    );
+    res.send(details);
+  } catch (error) {
+    res.send({ message: "Something went wrong" });
+  }
+});
+app.get("/episodes/:movieId/:seasonNum", async (req, res) => {
+  const { movieId, seasonNum } = req.params;
+
+  try {
+    const { data: details } = await axios.get(
+      `${IMOVIE_BASE_URL}/movies/${movieId}/season-files/${seasonNum}`
+    );
+    res.send(details);
+  } catch (error) {
+    res.send({ message: "Something went wrong" });
+  }
+});
+app.get("/video/:movieId/:episodeId", async (req, res) => {
+  const { episodeId, movieId } = req.params;
+  try {
+    let locat = "";
+    await axios.get(`${IMOVIE_BASE_URL}/movies/${movieId}/files/${episodeId}`, {
+      beforeRedirect(options, responseDetails) {
+        locat = responseDetails.headers.location;
+        res.send(locat);
+        return;
+      },
+    });
+    res.send("hello");
+  } catch (error) {
+    res.send({ message: "Something went wrong", error });
+  }
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
